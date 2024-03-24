bfb67837ed509a63ee34197a81e92c45179c0746
Yaël Guilloux <yael.guilloux@gmail.com>
2024-03-24 19:59:41 +0100

    chore(package): type module

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,12 +1,14 @@
-'use strict';
 
-const fs = require('fs');
-const path = require('path');
+
+import fs from 'node:fs'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
 
 const regex = /module\.exports = punycode;/;
 const output = 'export { ucs2decode, ucs2encode, decode, encode, toASCII, toUnicode };\nexport default punycode;';
+const dir = dirname(fileURLToPath(import.meta.url))
 
-const sourceContents = fs.readFileSync(path.resolve(__dirname, '../punycode.js'), 'utf-8');
+const sourceContents = fs.readFileSync(path.resolve(dir, '../punycode.js'), 'utf-8');
 
 if (!regex.test(sourceContents)) {
 	throw new Error('The underlying library has changed. Please update the prepublish script.');
@@ -14,4 +16,4 @@ if (!regex.test(sourceContents)) {
 
 const outputContents = sourceContents.replace(regex, output);
 
-fs.writeFileSync(path.resolve(__dirname, '../punycode.es6.js'), outputContents);
+fs.writeFileSync(path.resolve(dir, '../punycode.es6.js'), outputContents);
