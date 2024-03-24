0e07356fe2ea3108c28b9c97d3bef215316e89b1
Yaël Guilloux <yael.guilloux@gmail.com>
2024-03-24 20:01:39 +0100

    chore(package): fix type declarations

--- punycode.d.ts
+++ punycode.d.ts
@@ -1,24 +1,22 @@
-declare module 'punycode' {
-	function ucs2decode(string:string):Array<number>;
-	function ucs2encode(array:Array<number>):string;
-	function decode(string:string):string;
-	function encode(string:string):string;
-	function toASCII(string:string):string;
-	function toUnicode(string:string):string;
+declare function ucs2decode(string:string):Array<number>;
+declare function ucs2encode(array:Array<number>):string;
+declare function decode(string:string):string;
+declare function encode(string:string):string;
+declare function toASCII(string:string):string;
+declare function toUnicode(string: string): string;
 
-	interface Punycode {
-		'version': '2.3.1';
-		'ucs2': {
-			'decode': typeof ucs2decode;
-			'encode': typeof ucs2encode;
-		},
-		'decode': typeof decode;
-		'encode': typeof encode;
-		'toASCII': typeof toASCII;
-		'toUnicode': typeof toUnicode;
-	}
-
-	const punycode:Punycode;
-
-	export default punycode;
+interface Punycode {
+	'version': '2.3.1';
+	'ucs2': {
+		'decode': typeof ucs2decode;
+		'encode': typeof ucs2encode;
+	},
+	'decode': typeof decode;
+	'encode': typeof encode;
+	'toASCII': typeof toASCII;
+	'toUnicode': typeof toUnicode;
 }
+
+declare const punycode: Punycode;
+
+export default punycode;
